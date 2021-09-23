8f6cf2d48f366b2ca8cf7bd453d80b6702487beb
wea-linchao <linchao@we-architects.cn>
2021-09-23 17:01:40 +0800

    init

---
+++ src/components/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="index">
+    WEA Website
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'Hi'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.hello{
+  font-size:40px;
+}
+.index{
+  font-size: 40px;
+  font-weight: bolder;
+}
+
+</style>
